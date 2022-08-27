c26032013d6f103c924dad41b8ae8c91c3024bd8
TotiFlame <santifer4321@gmail.com>
2022-08-27 11:40:44 -0700

    Exercise 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,79 @@
 #include "sort.h"
 /**
+ * quick_sort - sorts an array of integers in ascensing
+ * order using the quick sort algorithm
  *
- *
+ * @array: array to search
+ * @size: size of the array
  */
 void quick_sort(int *array, size_t size)
 {
-  size_t i, j, pivote;
+	int start = 0, end = size - 1;
+	
+	if (!array || size == 1)
+		return;
+	quicksortfunc(array, size, start, end);
+}
 
-  for (i = 0; i < size;)
-  {
-  }
+void swap(int *fnum, int *snum)
+{
+	int tmp;
+
+	tmp = *snum;
+	*snum = *fnum;
+	*fnum = tmp;
+}
+
+int partition(int *array, int left, int right, int size)
+{
+	int pivot = array[right];
+	int i = left, n = 0;
+
+/**	printf("left: >>%d<<\n", left);
+*	printf("right >>%d<<\n", right);
+*	printf("partition\n");
+*/
+	while (i < right)
+	{
+/**		printf("pivot: >>%d<<\n", pivot);
+*		printf("n: >>%d<<\n", n);
+*		printf("i: >>%d<<\n", i);
+*		printf("array[i]: >>%d<<\n", array[i]);
+*		printf("right: >>%d<<\n\n\n", right);
+*/
+	if (array[i] > pivot)
+		{
+			n = i + 1;
+			while (n < right)
+			{
+				if (array[n] < pivot)
+				{
+					swap(&array[i], &array[n]);
+					print_array(array, size);
+					break;
+				}
+				n++;
+			}
+			if (pivot < array[i])
+			{
+				swap(&array[i], &array[right]);
+				print_array(array, size);
+			}
+			pivot = array[right];
+		}
+		i++;
+	}
+	return (i);
+}
+
+void quicksortfunc(int *array, int size, int left, int right)
+{
+	int part = 0;
+
+	if (left >= right)
+		return;
+
+	part = partition(array, left, right, size);
+	quicksortfunc(array, size, left, part - 1);
+	quicksortfunc(array, size, part + 1, right);
 }
--- sort.h
+++ sort.h
@@ -21,4 +21,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t *fnode, listint_t *snode, listint_t **list);
+int partition(int *array, int left, int right, int size);
+void swap(int *fnum, int *snum);
+void quick_sort(int *array, size_t size);
+void quicksortfunc(int *array, int size, int left, int right);
 #endif
