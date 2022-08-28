45eb254bb408e8473cf4dd7fa1f00fb7d3204908
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-28 17:03:33 -0300

    xd

--- sort.h
+++ sort.h
@@ -21,8 +21,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t *fnode, listint_t *snode, listint_t **list);
-
+void quicksortfunc(int *array, int size, int left, int right);
 void swap(int *fnum, int *snum);
-void quick_sort_r(int *a, size_t s, int f_s, int *f_a);
+int partition(int *array, int left, int right, int size);
 void quick_sort(int *array, size_t size);
 #endif
