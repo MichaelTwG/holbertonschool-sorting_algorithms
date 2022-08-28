1ac0c5888f1d992445962b90f49a18aa64345738
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-28 16:56:56 -0300

    test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 /**
- * swap_array - Swap two elements in a array
+ * swap - Swap two elements in a array
  * @first_value: a value of the array
  * @second_value: a valua of the array
  */
-void swap_array(int *first_value, int *second_value)
+void swap(int *first_value, int *second_value)
 {
 	int temp_value = 0;
 
@@ -28,10 +28,10 @@ void quick_sort(int *array, size_t size)
  * @full_size: full size of original array
  * @full_array: original array
  */
-void quick_sort_recursive(int *array, size_t size, int full_size, int *full_array)
+void quick_sort_r(int *array, size_t size, int full_size, int *full_array)
 {
 	int i = 0;
-	int j = 0;
+	int x = 0;
 	int has_swap = 0;
 	int pivot = 0;
 
@@ -41,11 +41,11 @@ void quick_sort_recursive(int *array, size_t size, int full_size, int *full_arra
 		if (array[i] > array[pivot])
 		{
 			has_swap = 0;
-			for (j = i + 1; j < pivot; j++)
+			for (x = i + 1; x < pivot; x++)
 			{
-				if (array[j] < array[pivot])
+				if (array[x] < array[pivot])
 				{
-					swap_array(&array[j], &array[i]);
+					swap(&array[x], &array[i]);
 					has_swap = 1;
 					print_array(full_array, full_size);
 					break;
@@ -53,7 +53,7 @@ void quick_sort_recursive(int *array, size_t size, int full_size, int *full_arra
 			}
 			if (!has_swap)
 			{
-				swap_array(&array[i], &array[pivot]);
+				swap(&array[i], &array[pivot]);
 				has_swap = 1;
 				pivot = i;
 				print_array(full_array, full_size);
@@ -63,9 +63,8 @@ void quick_sort_recursive(int *array, size_t size, int full_size, int *full_arra
 		i++;
 	}
 	if (pivot > 0)
-		quick_sort_recursive(array, pivot, full_size, full_array);
+		quick_sort_r(array, pivot, full_size, full_array);
 
 	if ((int)size - (pivot + 1) > 0)
-		quick_sort_recursive(&array[pivot + 1], size - (pivot + 1), full_size, full_array);
+		quick_sort_r(&array[pivot + 1], size - (pivot + 1), full_size, full_array);
 }
-
